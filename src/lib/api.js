e3aa81888dfdb8e1e47c2cc5c742f4ffd21649fa
Bhavya Parmar <bhavyaparmarw@gmail.com>
2026-04-01 23:49:34 +0530

    Update API base URL to use environment variable

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'sonner';
 
 const api = axios.create({
-  baseURL: "https://drivex-web-backend-production.up.railway.app" || '/api',
+  baseURL: import.meta.env.VITE_API_URL  || '/api',
   headers: {
     'Content-Type': 'application/json',
   },
