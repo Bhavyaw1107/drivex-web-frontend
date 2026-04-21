690d987995b599a514336419fd89357dc282b2f8
Bhavyaw1107 <bhavyaparmarw@gmail.com>
2026-04-21 14:04:19 +0530

    hard code

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter } from 'react-router-dom';
 
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const clerkPubKey = pk_test_ZnVubnktbGVvcGFyZC04Mi5jbGVyay5hY2NvdW50cy5kZXYk;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
