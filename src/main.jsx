f37cf5fa1f2fd996e3356e9a75be120a5502be6a
Bhavyaw1107 <bhavyaparmarw@gmail.com>
2026-04-22 20:46:03 +0530

    fix key

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const clerkPubKey = String(import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey}>
+    <ClerkProvider publishableKey="pk_test_ZnVubnktbGVvcGFyZC04Mi5jbGVyay5hY2NvdW50cy5kZXYk">
       <BrowserRouter
         future={{
           v7_startTransition: true,
